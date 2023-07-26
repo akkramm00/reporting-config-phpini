e6fda67611c34e745d1dd3b589aab3c53c25ba9f
akkramm00 <houssaine.elyaacoubi82@gmail.com>
2023-07-26 16:59:37 +0200

    regenerer le gestionnaire d'erreur par defaut

--- assets/index.php
+++ assets/index.php
@@ -21,5 +21,18 @@ try {
 }catch(Error $e) {
     echo 'Vous ne verrez jamais ce message car foopen n\'émét pas d\'erreurs de type Error! L\'erreur est déclenchée de manière classique' .PHP_EOL;
 }
+// Restaurer le gestionnaire d'erreur par defaut :
 
+set_error_handler(function($errno, $errstr, $errfile, $errline) {
+    if(!(error_reporting() & $errno)) {
+        // l'erreur n'est pas spécifiée dans la configuration du système de rapport d'erreur, donc on l'ignore
+        return;
+    }
+    echo"Nous sommes désolés , un problème vient de survenir: / \nNous vous intitons à revenir plus tard.". PHP_EOL;
+    die;
+});
+//votre code...
+
+//Restauration du gestionnaire d'erreur précédent
+restore_error_handler();
 ?>
