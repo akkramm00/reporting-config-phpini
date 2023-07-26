20c3f627cf434993d258d436104425a9aa144283
akkramm00 <houssaine.elyaacoubi82@gmail.com>
2023-07-26 13:36:28 +0200

    add all

---
+++ assets/index.php
@@ -0,0 +1,16 @@
+
+<?php
+
+/**
+* Démo 2 :
+*- comment configurer notre système de rapport d'erreurs (le reporting en PHP)
+* dans le fichier de config  php.ini
+* -servir notre notre code avec le serveur interne de php poour se placer dans un contexte web
+* - log une erreur avec la fonction error_log
+*/
+
+
+require 'foo.php' ;
+echo "<p> Cette instruction sera-t-elle visible ? ? </p>" . PHP_EOL;
+
+?>
